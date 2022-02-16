eb13c98cbc4d57008767aeb98a0c1664c7dee983
NickNocc <haretakis4@gmail.com>
2022-02-16 15:50:54 -0800

    added reply functionality

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,33 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+
+const ReplySchema = new Schema(
+  {
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    }
+  }
+);
+
 const CommentSchema = new Schema(
   {
   writtenBy: {
@@ -29,31 +56,7 @@ CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
 
-const ReplySchema = new Schema(
-  {
-    replyId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    replyBody: {
-      type: String
-    },
-    writtenBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    }
-  },
-  {
-    toJSON: {
-      getters: true,
-      virtuals: true
-    }
-  }
-);
+
 
 const Comment = model('Comment', CommentSchema);
 
