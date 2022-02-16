96c0ff09f4e79ddb28713b51fe3c7a0d8f2ba9b3
NickNocc <haretakis4@gmail.com>
2022-02-16 15:01:49 -0800

    added comment functionality

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -10,12 +10,13 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 let pizzaId;
 
 function getPizza() {
+  // get id of pizza
   const searchParams = new URLSearchParams(document.location.search.substring(1));
   const pizzaId = searchParams.get('id');
 
+  // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
     .then(response => {
-      console.log(response);
       if (!response.ok) {
         throw new Error({ message: 'Something went wrong!' });
       }
@@ -28,7 +29,7 @@ function getPizza() {
       alert('Cannot find a pizza with this id! Taking you back.');
       window.history.back();
     });
-}
+};
 
 function printPizza(pizzaData) {
   console.log(pizzaData);
@@ -108,6 +109,28 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function handleNewReplySubmit(event) {
